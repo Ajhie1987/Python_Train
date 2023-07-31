3de147a115fc0188589a82e612751ac07463ef0d
ajhie <fajri.sofianto@btn.co.id>
2023-07-31 22:03:23 +0700

    update final

---
+++ TestModul.py
@@ -0,0 +1,20 @@
+nama = "fajri sofianto"
+
+umur = 30
+
+background = {
+    "edukasi": "S1",
+    "keluarga": "berkeluarga",
+    "pekerjaan": "Data Analytic Officer",
+    "tahun lahir": 1987
+}
+
+def kenalan():
+    print("Nama saya",nama)
+    print("umur",umur)
+
+def aritmatik():
+    a = 1
+    while(a<5):
+        print("Nilai",a)
+        a = a + 1
